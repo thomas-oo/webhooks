03bddf6c89c234e217bae9da4d33af21f4311818
Chady Kamar <chady.kamar@gmail.com>
2017-02-12 23:05:57 -0500

    Fix typo

--- fb_mcbot/views.py
+++ fb_mcbot/views.py
@@ -9,10 +9,10 @@ from pprint import pprint
 
 PAGE_ACCESS_TOKEN = 'EAATYvrVIHsIBADoFlddL8Aqeu9YiXCkzBehGuqGBzsl2dM22QTvsNH2AuBpkaWPGfnIifWDNJYUlhbo7E8M15amsnBDtZBZC1WFohgYVg5HJ2zA1vKZBOthJZBwQPbX6TzMSpOfXCQxfrbPMjyh40nC5o6yQBm1xDpBpIvlKxwZDZD'
 
-def post_facebook_message(fbid, recevied_message):
+def post_facebook_message(fbid, received_message):
     # TODO Change access token           
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token={}'.format(PAGE_ACCESS_TOKEN)
-    response_msg = dumps({"recipient":{"id":fbid}, "message":{"text":recevied_message}})
+    response_msg = dumps({"recipient":{"id":fbid}, "message":{"text":received_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
