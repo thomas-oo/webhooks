a7afd5504ba189e084a1c2d909f397f425bd84ef
Chady Kamar <chady.kamar@gmail.com>
2017-02-13 13:12:01 -0500

    Switch to permanent page access token

--- fb_mcbot/views.py
+++ fb_mcbot/views.py
@@ -4,15 +4,20 @@ from django.views.decorators.csrf import csrf_exempt
 
 import requests
 
+import facebook
+
 from json import dumps, loads
 from pprint import pprint
 
+
 APP_ID = '362191170829320'
 APP_SECRET = 'b89bdf1697d83f88c606bbffbc8487bb'
+# Permanent page access token
+PAGE_ACCESS_TOKEN = 'EAAFJaTQTZCAgBAKquTNCRxUz2edEmSuocZC9EreThZAiGFImZAHGGqVlcLdvRDe3vMNXZBvXnQL3ZC0VMJY8IMAWt6t0j8tZCBwsYBZAQDEYOh1kwdjzmnu6zkg1tPyWITWTtgcQvZAw0mvyqZAIkZABmmkMQr1UQwBCkKkcXCTmaZAFHAZDZD'
 
 def post_facebook_message(fbid, received_message):
     # TODO Change access token           
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token={}|{}'.format(APP_ID, APP_SECRET)
+    post_message_url = 'https://graph.facebook.com/v2.8/me/messages?access_token={}'.format(PAGE_ACCESS_TOKEN)
 
     response_msg = dumps({"recipient":{"id":fbid}, "message":{"text":received_message}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
